3d95577968b94bdaeaf3ba1c9f6511bb93fcb0b5
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-29 22:31:30 +0600

    image route declare

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const PORT = process.env.PORT || 5000;
 const colors = require("colors");
 const mongoose = require('mongoose');
 require('dotenv').config();
-const multer  = require('multer');
+const multer = require('multer');
 
 
-app.use(cors());
+app.use(cors({origin:true, credentials:true}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.json());
+
+app.use('/public', express.static('public'))
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
 
 
 // import routes
--- v1/Middlewares/Uploader.js
+++ v1/Middlewares/Uploader.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: "Images/",
+    destination: "public/images",
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, uniqueSuffix + '-' + file.originalname )
