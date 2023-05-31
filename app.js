05b1ec25fe79f6d3baeb7089207c0cfeec201f27
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-31 20:09:25 +0600

    add cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use('/public', express.static('public'))
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://publicationsbd1.web.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Credentials', true);
