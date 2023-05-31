3a8cf457305757aad62d68d92e42f6c4cbe7a338
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-31 20:18:02 +0600

    add cors p

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.json());
 
 app.use('/public', express.static('public'))
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', '*');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// })
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
 
 
 // import routes
